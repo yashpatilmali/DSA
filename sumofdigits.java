70e80b64afee3eca9a4be7988f13ecc9daf19f2a
yashpatilmali <yashsmali91@gmail.com>
2026-03-23 18:02:59 +0530

    23-03-2026

---
+++ sumofdigits.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class sumofdigits { 
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int sum =0;
+
+        while (n!=0){
+            sum = n%10 +sum;
+            n = n/10;
+
+        }
+
+        System.out.println(sum);
+    }
+    
+}
